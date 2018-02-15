9a50a41c2d8eb36b4650d9d85efc9bb9dd054fd7
Hillary Sousa <hillaryhpf@gmail.com>
2018-02-15 12:04:02 -0200

    Criado reducer para livros.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return  [
+        {title: 'JavaScript: The Good Parts'},
+        {title: 'Harry Potter and the Philosopher\'s Stone'},
+        {title: 'Percy Jackson and the Sea of Monsters'},
+        {title: 'The Handmaid\'s Tale'}
+    ]
+}
